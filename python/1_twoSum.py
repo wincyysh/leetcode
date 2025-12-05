f4d20e904e4e0404ae076eb88bff868ffb2a5977
wincyH <wincy.yingshi.huang@gmail.com>
2025-12-05 23:15:32 +0100

    linked list cycle

--- python/1_twoSum.py
+++ python/1_twoSum.py
@@ -1,26 +1,74 @@
-class Solution(object):
+import unittest
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         >>> test = Solution()
+#         >>> test.twoSum([2,7,11,15], 9)
+#         [0,1]
+#         >>> test.twoSum([3,2,4], 6)
+#         [1,2]
+#         >>> test.twoSum([3,3], 6)
+#         [0,1]
+#         """
+#         lnum = len(nums) - 1
+#         while lnum > 0:
+#             i = lnum
+#             for j in range(lnum):
+#                 if target - nums[j] == nums[i]:
+#                     return [j,i]
+#             i -= 1
+#             lnum = i
+                
+#         return []
+
+class Solution:
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        >>> test = Solution()
-        >>> test.twoSum([2,7,11,15], 9)
-        [0,1]
-        >>> test.twoSum([3,2,4], 6)
-        [1,2]
-        >>> test.twoSum([3,3], 6)
-        [0,1]
-        """
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+        # If no valid pair is found, return an empty list
+        return []
 
+# class Solution:
+#     def twoSum(self, nums, target):
+#         hashmap = {}
+#         for i in range(len(nums)):
+#             complement = target - nums[i]
+#             if complement in hashmap:
+#                 return [i, hashmap[complement]]
+#             hashmap[nums[i]] = i
+#         # Return an empty list if no solution is found
+#         return []
 
+class TesttwoSum(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+    
+    def test_case1(self):
+        nums = [2,7,11,15]
+        target = 9
+        result = [0,1]
+        self.assertEqual(result, self.solution.twoSum(nums, target))
 
+    def test_case2(self):
+        nums = [3,2,4]
+        target = 6
+        result = [1,2]
+        self.assertEqual(result, self.solution.twoSum(nums, target))
 
-# nums = [2,7,11,15], target = 9
-# [0,1]
+    def test_case3(self):
+        nums = [3,3]
+        target = 6
+        result = [0,1]
+        self.assertEqual(result, self.solution.twoSum(nums, target))
 
-# nums = [3,2,4], target = 6
-# [1,2]
-
-# nums = [3,3], target = 6
-# [0,1]
+if __name__ == '__main__':
+    unittest.main()
--- python/30_substringWithConcatenationOfAllWords.py
+++ python/30_substringWithConcatenationOfAllWords.py
@@ -1,36 +1,64 @@
 import unittest
 
-class Solution(object):
+class Solution:
     def findSubstring(self, s, words):
         """
         :type s: str
         :type words: List[str]
         :rtype: List[int]
         """
-        output = []
-        return output
+        n = len(s)
+        k = len(words)
+        word_length = len(words[0])
+        substring_size = word_length * k
+        word_count = collections.Counter(words)
+
+        def check(i):
+            # Copy the original dictionary to use for this index
+            remaining = word_count.copy()
+            words_used = 0
+
+            # Each iteration will check for a match in words
+            for j in range(i, i + substring_size, word_length):
+                sub = s[j : j + word_length]
+                if remaining[sub] > 0:
+                    remaining[sub] -= 1
+                    words_used += 1
+                else:
+                    break
+
+            # Valid if we used all the words
+            return words_used == k
+
+        answer = []
+        for i in range(n - substring_size + 1):
+            if check(i):
+                answer.append(i)
+
+        return answer
 
 class testFindSubstringFunction(unittest.TestCase):
-    def setUp(self):
-        self.solution = Solution()
-    
-    def testCase1(self):
-        s = "barfoothefoobarman"
-        words = ["foo","bar"]
-        result = [0,9]
-        self.assertEqual(result, self.solution.findSubstring(s, words))
-    
-    def testCase2(self):
-        s = "wordgoodgoodgoodbestword"
-        words = ["word","good","best","word"]
-        result = []
-        self.assertEqual(result, self.solution.findSubstring(s, words))
+  def setUp(self):
+    self.solution = Solution()
+  
+  def testCase1(self):
+    s = "barfoothefoobarman"
+    words = ["foo","bar"]
+    result = [0,9]
+    self.assertEqual(result, self.solution.findSubstring(s, words))
+  
+  def testCase2(self):
+    s = "wordgoodgoodgoodbestword"
+    words = ["word","good","best","word"]
+    result = []
+    self.assertEqual(result, self.solution.findSubstring(s, words))
 
-    def testCase3(self):
-        s = "barfoofoobarthefoobarman"
-        words = ["bar","foo","the"]
-        result = [6,9,12]
-        self.assertEqual(result, self.solution.findSubstring(s, words))
+  def testCase3(self):
+    s = "barfoofoobarthefoobarman"
+    words = ["bar","foo","the"]
+    result = [6,9,12]
+    self.assertEqual(result, self.solution.findSubstring(s, words))
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
+
